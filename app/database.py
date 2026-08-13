4f0ebbf29164914368a2c53fd136651329063ac9
Garvit Agrawal <garvitagrawal75@gmail.com>
2026-08-13 21:47:42 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 
-engine = create_async_engine(settings.database_url, echo=False)
+engine = create_async_engine(settings.database_url, echo=False, pool_pre_ping=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
