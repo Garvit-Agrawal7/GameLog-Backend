8b33bdac26c7fe8296ac3153f7249149c2344492
Garvit Agrawal <garvitagrawal75@gmail.com>
2026-08-21 19:55:11 +0530

    Update xbox_auth.py

--- app/xbox_auth.py
+++ app/xbox_auth.py
@@ -20,8 +20,7 @@ class XboxAuth:
                     self.xbox_url + "/app/claim",
                     json={"code": code, "app_key": self.app_key},
                     headers={
-                            "Content-Type": "application/json",
-                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+                            "Content-Type": "application/json"
                         },
                 )
         except httpx.TimeoutException:
